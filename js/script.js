41a4dacac5b276b37d45500852f6bb2a4bb804e7
sohibnasser <92693350+sohibnasser@users.noreply.github.com>
2022-01-01 19:40:56 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,238 @@
+
+
+
+/*--------------------------------- navbar -------------------------------------*/
+
+
+/*--------- one ---------*/
+function one(){
+  "use strict";
+  document.getElementById("one").style.backgroundColor ="red";
+  document.getElementById("one").style.padding = "5px",
+  document.getElementById("one").style.borderRadius = "5px";
+};
+function on(){
+  "use strict";
+  document.getElementById("one").style.backgroundColor ="";
+  document.getElementById("one").style.padding = "",
+  document.getElementById("one").style.borderRadius = "";
+};
+
+
+/*--------- two ---------*/
+function two(){
+  "use strict";
+  document.getElementById("two").style.backgroundColor ="red";
+  document.getElementById("two").style.padding = "5px",
+  document.getElementById("two").style.borderRadius = "5px";
+};
+function tw(){
+  "use strict";
+  document.getElementById("two").style.backgroundColor ="";
+  document.getElementById("two").style.padding = "",
+  document.getElementById("two").style.borderRadius = "";
+};
+
+
+/*--------- three ---------*/
+function three(){
+  "use strict";
+  document.getElementById("three").style.backgroundColor ="red";
+  document.getElementById("three").style.padding = "5px",
+  document.getElementById("three").style.borderRadius = "5px";
+};
+function th(){
+  "use strict";
+  document.getElementById("three").style.backgroundColor ="";
+  document.getElementById("three").style.padding = "",
+  document.getElementById("three").style.borderRadius = "";
+};
+
+
+/*--------- four ---------*/
+function four(){
+  "use strict";
+  document.getElementById("four").style.backgroundColor ="red";
+  document.getElementById("four").style.padding = "5px",
+  document.getElementById("four").style.borderRadius = "5px";
+};
+function fo(){
+  "use strict";
+  document.getElementById("four").style.backgroundColor ="";
+  document.getElementById("four").style.padding = "",
+  document.getElementById("four").style.borderRadius = "";
+};
+
+
+/*--------- five ---------*/
+function five(){
+  "use strict";
+  document.getElementById("five").style.backgroundColor ="red";
+  document.getElementById("five").style.padding = "5px",
+  document.getElementById("five").style.borderRadius = "5px";
+};
+function fi(){
+  "use strict";
+  document.getElementById("five").style.backgroundColor ="";
+  document.getElementById("five").style.padding = "",
+  document.getElementById("five").style.borderRadius = "";
+};
+
+/*--------------------------------- about -------------------------------------*/
+
+
+
+
+/*
+  if(document.body.scrollTop >= 200){
+
+      document.querySelector("button").style.visibility ="visible"
+      document.querySelector("button").style.position ="fixed";
+  }
+  else{
+      document.querySelector("button").style.visibility ="hidden";
+  }
+  
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*--------------------------------- our-works -------------------------------------*/
+
+function show1(){
+  "use strict";
+  document.getElementById("click1").style.visibility = "visible";
+}
+function hide1(){
+  "use strict";
+  document.getElementById("click1").style.visibility = "hidden";
+}
+
+function show2(){
+  "use strict";
+  document.getElementById("click2").style.visibility = "visible";
+}
+function hide2(){
+  "use strict";
+  document.getElementById("click2").style.visibility = "hidden";
+}
+
+function show3(){
+  "use strict";
+  document.getElementById("click3").style.visibility = "visible";
+}
+function hide3(){
+  "use strict";
+  document.getElementById("click3").style.visibility = "hidden";
+}
+
+function show4(){
+  "use strict";
+  document.getElementById("click4").style.visibility = "visible";
+}
+function hide4(){
+  "use strict";
+  document.getElementById("click4").style.visibility = "hidden";
+}
+
+function show5(){
+  "use strict";
+  document.getElementById("click5").style.visibility = "visible";
+}
+function hide5(){
+  "use strict";
+  document.getElementById("click5").style.visibility = "hidden";
+}
+
+function show6(){
+  "use strict";
+  document.getElementById("click6").style.visibility = "visible";
+}
+function hide6(){
+  "use strict";
+  document.getElementById("click6").style.visibility = "hidden";
+}
+
+function show7(){
+  "use strict";
+  document.getElementById("click7").style.visibility = "visible";
+}
+function hide7(){
+  "use strict";
+  document.getElementById("click7").style.visibility = "hidden";
+}
+
+function show8(){
+  "use strict";
+  document.getElementById("click8").style.visibility = "visible";
+}
+function hide8(){
+  "use strict";
+  document.getElementById("click8").style.visibility = "hidden";
+}
+
+function show9(){
+  "use strict";
+  document.getElementById("click9").style.visibility = "visible";
+}
+function hide9(){
+  "use strict";
+  document.getElementById("click9").style.visibility = "hidden";
+}
+
+
+
+
+
+
+/*--------------------------------- contact-us -------------------------------------*/
+
+
+
+
+function map(){
+  "use strict";
+  window.open("https://www.google.de/maps/@51.1758057,10.4541194,6z,_blank");
+}
+
+
+function gm(){
+  "use strict";
+  window.open("https://www.google.com/intl/de/gmail/about/#,_blank");
+}
+
+
+
+
+document.getElementById("fac").onclick=function(){
+  "use strict";
+  window.location.href="https://www.facebook.com/login/";
+}
+
+document.getElementById("twi").onclick=function(){
+  "use strict";
+  window.location.href="https://twitter.com/login";
+
+}
+
+
+document.getElementById("you").onclick=function(){
+  "use strict";
+  window.location.href="https://www.youtube.com/?gl=DE&hl=de";
+}
+
+
+document.getElementById("ins").onclick=function(){
+  "use strict";
+  window.location.href="https://www.instagram.com/";
+}
+
